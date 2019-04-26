d2bce618b766f91febb7785da0053c6e6b65100b
rbagrowski <rbagrowsk@gmail.com>
2019-04-26 23:22:37 +0200

    Updates

---
+++ precache-manifest.d1a7a7e216f9bba66d44510b9538d5be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a9c17acf2888d6c372f21a40f8b8857",
+    "url": "/easy-cv/static/media/logo.1a9c17ac.jpg"
+  },
+  {
+    "revision": "74f20d61c68d1e91ed49",
+    "url": "/easy-cv/static/js/runtime~main.cef27d67.js"
+  },
+  {
+    "revision": "a91f72fbdba3fa8daeac",
+    "url": "/easy-cv/static/js/main.50d7cec0.chunk.js"
+  },
+  {
+    "revision": "7a6074929b440cb793cf",
+    "url": "/easy-cv/static/js/2.65a0ee3e.chunk.js"
+  },
+  {
+    "revision": "a91f72fbdba3fa8daeac",
+    "url": "/easy-cv/static/css/main.70a9b034.chunk.css"
+  },
+  {
+    "revision": "7a6074929b440cb793cf",
+    "url": "/easy-cv/static/css/2.3b14c35a.chunk.css"
+  },
+  {
+    "revision": "755506e46dab0773d94ed2aba8df2008",
+    "url": "/easy-cv/index.html"
+  }
+];
