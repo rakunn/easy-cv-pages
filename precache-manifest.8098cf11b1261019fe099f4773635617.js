43058d6916918c606b46101b784cc9a7de2efed9
rbagrowski <rbagrowsk@gmail.com>
2020-10-14 18:11:46 +0200

    Updates

---
+++ precache-manifest.8098cf11b1261019fe099f4773635617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e88ae05661e6d734bf",
+    "url": "/easy-cv/static/css/main.0cd16698.chunk.css"
+  },
+  {
+    "revision": "51e88ae05661e6d734bf",
+    "url": "/easy-cv/static/js/main.938162f9.chunk.js"
+  },
+  {
+    "revision": "74f20d61c68d1e91ed49",
+    "url": "/easy-cv/static/js/runtime~main.cef27d67.js"
+  },
+  {
+    "revision": "3c208fd01134c8852b30",
+    "url": "/easy-cv/static/css/2.3f65337e.chunk.css"
+  },
+  {
+    "revision": "3c208fd01134c8852b30",
+    "url": "/easy-cv/static/js/2.ef9bb131.chunk.js"
+  },
+  {
+    "revision": "1a9c17acf2888d6c372f21a40f8b8857",
+    "url": "/easy-cv/static/media/logo.1a9c17ac.jpg"
+  },
+  {
+    "revision": "8575c91ae08051c1f31d401c51db63bc",
+    "url": "/easy-cv/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/easy-cv/precache-manifest.6857c8fcc1cbc829ba6927dffbd64247.js"
+  "/easy-cv/precache-manifest.8098cf11b1261019fe099f4773635617.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/easy-cv/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
